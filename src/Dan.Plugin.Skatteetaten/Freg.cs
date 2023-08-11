7c193bdaecf80088fcdd605ce6551bf9b2610a2b
erlend oksvoll <erlend.oksvoll@digdir.no>
2023-08-11 09:42:36 +0200

    corrected url for dgm freg

--- src/Dan.Plugin.Skatteetaten/Freg.cs
+++ src/Dan.Plugin.Skatteetaten/Freg.cs
@@ -48,7 +48,7 @@ namespace Dan.Plugin.Skatteetaten
             //servicecontextname, url
             serviceContextRightsPkg.Add(new KeyValuePair<string, string>("DigitaleHelgeland", $"{ENV}folkeregisteret/offentlig-med-hjemmel/api/v1/personer/{PERSON}?part={PART}"));
             serviceContextRightsPkg.Add(new KeyValuePair<string, string>("Reelle rettighetshavere", $"{ENV}folkeregisteret/api/offentligutenhjemmel/v1/personer/{PERSON}"));
-            serviceContextRightsPkg.Add(new KeyValuePair<string, string>("DigitalGravferdsmelding", $"{ENV}folkeregisteret/api/offentlig-med-hjemmel/v1/personer/{PERSON}?part={PART}"));
+            serviceContextRightsPkg.Add(new KeyValuePair<string, string>("DigitalGravferdsmelding", $"{ENV}folkeregisteret/offentlig-med-hjemmel/api/v1/personer/{PERSON}?part={PART}"));
             serviceContextRightsPkg.Add(new KeyValuePair<string, string>("OED", $"{ENV}folkeregisteret/offentlig-med-hjemmel/api/v1/personer/{PERSON}"));
         }
 
