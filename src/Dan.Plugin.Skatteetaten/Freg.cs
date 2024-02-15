149f5c40702d23d725f32e183e8d6efdb1977bad
erlend oksvoll <erlend.oksvoll@digdir.no>
2024-02-15 13:58:06 +0100

    added part for reelle rettighetshavere

--- src/Dan.Plugin.Skatteetaten/Freg.cs
+++ src/Dan.Plugin.Skatteetaten/Freg.cs
@@ -47,7 +47,7 @@ namespace Dan.Plugin.Skatteetaten
 
             //servicecontextname, url
             serviceContextRightsPkg.Add(new KeyValuePair<string, string>("DigitaleHelgeland", $"{ENV}folkeregisteret/offentlig-med-hjemmel/api/v1/personer/{PERSON}?part={PART}"));
-            serviceContextRightsPkg.Add(new KeyValuePair<string, string>("Reelle rettighetshavere", $"{ENV}folkeregisteret/api/offentligutenhjemmel/v1/personer/{PERSON}"));
+            serviceContextRightsPkg.Add(new KeyValuePair<string, string>("Reelle rettighetshavere", $"{ENV}folkeregisteret/api/offentligutenhjemmel/v1/personer/{PERSON}?part={PART}"));
             serviceContextRightsPkg.Add(new KeyValuePair<string, string>("DigitalGravferdsmelding", $"{ENV}folkeregisteret/offentlig-med-hjemmel/api/v1/personer/{PERSON}?part={PART}"));
             serviceContextRightsPkg.Add(new KeyValuePair<string, string>("OED", $"{ENV}folkeregisteret/offentlig-med-hjemmel/api/v1/personer/{PERSON}"));
         }
